bda89b6e570e1c3ef7a267fae30cd59c7a8da990
fab4u_4empion <reserv390@gmail.com>
2022-09-11 12:07:43 +0300

    add basic chat list layout

--- src/panels/messages.js
+++ src/panels/messages.js
@@ -1,14 +1,29 @@
 import { Icon28MessageOutline } from "@vkontakte/icons"
-import { Group, PanelHeader, PanelHeaderBack, Placeholder } from "@vkontakte/vkui"
+import { Avatar, calcInitialsAvatarColor, Caption, Group, InitialsAvatar, List, PanelHeader, PanelHeaderBack, Placeholder, RichCell, SimpleCell, Text } from "@vkontakte/vkui"
+import { useShortText } from "../hooks/useShortText"
 
 export const Messages = () => {
     return (
         <>
             <PanelHeader>Сообщения</PanelHeader>
-            <Group style={{ height: "1000px" }}>
-                <Placeholder
-                    icon={<Icon28MessageOutline width={56} height={56} />}
-                ></Placeholder>
+            <Group>
+                <List>
+                    {
+                        [1, 2, 3, 4].map(e => {
+                            return (
+                                <RichCell
+                                    before={<InitialsAvatar gradientColor={calcInitialsAvatarColor(e)}>C{e}</InitialsAvatar>}
+                                    key={e}
+                                    caption={
+                                        <Text>{useShortText("FirstName: last message very long text", 30)} &#183; 2h</Text>
+                                    }
+                                >
+                                    Chat {e}
+                                </RichCell>
+                            )
+                        })
+                    }
+                </List>
             </Group>
         </>
     )
