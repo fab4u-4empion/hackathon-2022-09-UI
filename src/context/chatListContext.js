3eeaca65dbddda7346e9df470634df6b29553eb3
fab4u_4empion <reserv390@gmail.com>
2022-09-12 20:37:50 +0300

    add dynamic pagination

--- src/context/chatListContext.js
+++ src/context/chatListContext.js
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+const axios = require('axios');
 
 const Context = createContext()
 
@@ -7,8 +8,53 @@ export const useChatListContextProvider = () => {
 }
 
 export const ChatListContextProvider = ({children}) => {
+
+    const [chats, setChats] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [totalCount, setTotalCount] = useState(0)
+    const [limit] = useState(15)
+    const [fetching, setFetching] = useState(true)
+    const [endOfPage, setEndOfPage] = useState(false)
+
+    useEffect(() => {
+        if (fetching) {
+            axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${currentPage}`)
+                .then(response => {
+                    setChats([...chats, ...response.data])
+                    setCurrentPage(prev => prev + 1)
+                    setTotalCount(response.headers["x-total-count"])
+                })
+                .finally(() => {
+                    setEndOfPage(false)
+                    setFetching(false)
+                })
+        }
+    }, [fetching])
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler)
+        return function () {
+            window.removeEventListener("scroll", scrollHandler)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (endOfPage && chats.length < 100) {
+            setFetching(true)
+        }
+    }, [endOfPage])
+
+    const scrollHandler = (e) => {
+        if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+            setEndOfPage(true)
+        }
+    }
+
     return (
-        <Context.Provider>
+        <Context.Provider value={{
+            chats,
+            fetching
+        }}>
             {children}
         </Context.Provider>
     )
