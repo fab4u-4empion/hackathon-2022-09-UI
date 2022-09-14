59800c9843d254f1cac8e9227b2551d8103081e4
fab4u_4empion <reserv390@gmail.com>
2022-09-14 22:48:44 +0300

    fixes

--- src/panels/signUp.js
+++ src/panels/signUp.js
@@ -1,4 +1,6 @@
-import { Group, PanelHeader, View, Panel, Button, FormLayout, FormItem, Input, FormLayoutGroup, DatePicker, Select, } from "@vkontakte/vkui"
+import { Group, PanelHeader, View, Panel, Button, FormLayout, FormItem, Input, FormLayoutGroup, DatePicker, Select, Textarea, Checkbox, Link, ConfigProvider, AdaptivityProvider, AppRoot, SplitLayout, } from "@vkontakte/vkui"
+import React from "react";
+import '@vkontakte/vkui/dist/vkui.css';
 
 class SignUp extends React.Component {
 
@@ -38,101 +40,112 @@ class SignUp extends React.Component {
         const { email, purpose, showPatronym } = this.state;
 
         return (
-            <View activePanel="new-user">
-                <Panel id="new-user">
-                    <PanelHeader>Регистрация</PanelHeader>
-                    <Group>
-                        <FormLayout>
-                            <FormItem
-                                top="E-mail"
-                                status={email ? "valid" : "error"}
-                                bottom={
-                                    email
-                                        ? "Электронная почта введена верно!"
-                                        : "Пожалуйста, введите электронную почту"
-                                }
-                            >
-                                <Input
-                                    type="email"
-                                    name="email"
-                                    value={email}
-                                    onChange={this.onChange}
-                                />
-                            </FormItem>
+            <ConfigProvider>
+                <AdaptivityProvider>
+                    <AppRoot>
+                        <SplitLayout
+                            header={<PanelHeader separator={false} />}
+                            style={{ justifyContent: "center" }}
+                        >
+                            <View activePanel="new-user">
+                                <Panel id="new-user">
+                                    <PanelHeader>Регистрация</PanelHeader>
+                                    <Group>
+                                        <FormLayout>
+                                            <FormItem
+                                                top="E-mail"
+                                                status={email ? "valid" : "error"}
+                                                bottom={
+                                                    email
+                                                        ? "Электронная почта введена верно!"
+                                                        : "Пожалуйста, введите электронную почту"
+                                                }
+                                            >
+                                                <Input
+                                                    type="email"
+                                                    name="email"
+                                                    value={email}
+                                                    onChange={this.onChange}
+                                                />
+                                            </FormItem>
 
-                            <FormLayoutGroup >
-                                <FormItem top="Никнейм">
-                                    <Input />
-                                </FormItem>
-                            </FormLayoutGroup>
+                                            <FormLayoutGroup >
+                                                <FormItem top="Никнейм">
+                                                    <Input />
+                                                </FormItem>
+                                            </FormLayoutGroup>
 
-                            <FormItem top="Пароль">
-                                <Input type="password" placeholder="Введите пароль" />
-                            </FormItem>
+                                            <FormItem top="Пароль">
+                                                <Input type="password" placeholder="Введите пароль" />
+                                            </FormItem>
 
-                            <FormItem bottom="Пароль может содержать только латинские буквы и цифры.">
-                                <Input type="password" placeholder="Повторите пароль" />
-                            </FormItem>
+                                            <FormItem bottom="Пароль может содержать только латинские буквы и цифры.">
+                                                <Input type="password" placeholder="Повторите пароль" />
+                                            </FormItem>
 
-                            <FormLayoutGroup>
-                                <FormItem top="Номер телефона">
-                                    <Input type="phone" placeholder="+375290000000" />
-                                </FormItem>
-                            </FormLayoutGroup>
+                                            <FormLayoutGroup>
+                                                <FormItem top="Номер телефона">
+                                                    <Input type="phone" placeholder="+375290000000" />
+                                                </FormItem>
+                                            </FormLayoutGroup>
 
-                            <FormItem top="Дата рождения">
-                                <DatePicker
-                                    min={{ day: 1, month: 1, year: 1901 }}
-                                    max={{ day: 31, month: 12, year: 2022 }}
-                                    dayPlaceholder="ДД"
-                                    monthPlaceholder="MM"
-                                    yearPlaceholder="ГГ"
-                                />
-                            </FormItem>
+                                            <FormItem top="Дата рождения">
+                                                <DatePicker
+                                                    min={{ day: 1, month: 1, year: 1901 }}
+                                                    max={{ day: 31, month: 12, year: 2022 }}
+                                                    dayPlaceholder="ДД"
+                                                    monthPlaceholder="MM"
+                                                    yearPlaceholder="ГГ"
+                                                />
+                                            </FormItem>
 
-                            <FormItem top="Пол">
-                                <Select
-                                    placeholder="Выберите пол"
-                                    options={[
-                                        {
-                                            value: "1",
-                                            label: "Мужской",
-                                        },
-                                        {
-                                            value: "2",
-                                            label: "Женский",
-                                        },
-                                        {
-                                            value: "3",
-                                            label: "Другой",
-                                        },
-                                    ]}
-                                />
-                            </FormItem>
+                                            <FormItem top="Пол">
+                                                <Select
+                                                    placeholder="Выберите пол"
+                                                    options={[
+                                                        {
+                                                            value: "1",
+                                                            label: "Мужской",
+                                                        },
+                                                        {
+                                                            value: "2",
+                                                            label: "Женский",
+                                                        },
+                                                        {
+                                                            value: "3",
+                                                            label: "Другой",
+                                                        },
+                                                    ]}
+                                                />
+                                            </FormItem>
 
-                            {this.addressItems.map(({ label, name }) => (
-                                <FormItem top={label} key={name}>
-                                    <Input name={name} />
-                                </FormItem>
-                            ))}
+                                            {this.addressItems.map(({ label, name }) => (
+                                                <FormItem top={label} key={name}>
+                                                    <Input name={name} />
+                                                </FormItem>
+                                            ))}
 
-                            <FormItem top="О себе">
-                                <Textarea />
-                            </FormItem>
-                            <Checkbox>
-                                Согласен со всем <Link>этим</Link>
-                            </Checkbox>
-                            <FormItem>
-                                <Button size="l" stretched>
-                                    Зарегистрироваться
-                                </Button>
-                            </FormItem>
-                        </FormLayout>
-                    </Group>
-                </Panel>
-            </View>
+                                            <FormItem top="О себе">
+                                                <Textarea />
+                                            </FormItem>
+                                            <Checkbox>
+                                                Согласен со всем <Link>этим</Link>
+                                            </Checkbox>
+                                            <FormItem>
+                                                <Button size="l" stretched>
+                                                    Зарегистрироваться
+                                                </Button>
+                                            </FormItem>
+                                        </FormLayout>
+                                    </Group>
+                                </Panel>
+                            </View>
+                        </SplitLayout>
+                    </AppRoot>
+                </AdaptivityProvider>
+            </ConfigProvider>
         );
     }
 }
 
-<Example />;
+export default SignUp
