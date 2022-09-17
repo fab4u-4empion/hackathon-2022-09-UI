6c99a220f52569c95e2c256655fbae145681421d
ArtemKonchyts <artem.kon2003@mail.ru>
2022-09-17 19:33:06 +0300

    Create event layout

--- src/panels/events.js
+++ src/panels/events.js
@@ -1,14 +1,26 @@
-import { Icon56NewsfeedOutline } from "@vkontakte/icons"
-import { Group, PanelHeader, PanelHeaderBack, Placeholder } from "@vkontakte/vkui"
+import { Group, PanelHeader, CardGrid, Card, div, Title, Text, Caption, Button } from "@vkontakte/vkui"
 
 export const Events = () => {
     return (
         <>
             <PanelHeader>События</PanelHeader>
             <Group style={{ height: "1000px" }}>
-                <Placeholder
-                    icon={<Icon56NewsfeedOutline width={56} height={56} />}
-                />
+                <CardGrid size="l">
+                    <Card mode="shadow">
+                        <div style={{ height: 100 }}>
+                            <Title level="2" style={{ marginBottom: 15 }}>
+                                Название
+                            </Title>
+                            <Text style={{ marginBottom: 15 }}>Описание</Text>
+                            <Caption level="3">
+                                01.01.2020
+                            </Caption>
+                        </div>
+                        <Button stretched mode="secondary" size="s">
+                            Добавить
+                        </Button>
+                    </Card>
+                </CardGrid>
             </Group>
         </>
     )
