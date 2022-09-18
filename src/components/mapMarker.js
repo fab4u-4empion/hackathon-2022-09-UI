3a1330920227e9c377983e45ceefd083efbe43a8
fab4u_4empion <reserv390@gmail.com>
2022-09-18 16:29:57 +0300

    add marker basic layout

---
+++ src/components/mapMarker.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Icon24Add } from '@vkontakte/icons';
+
+export const MapMarker = () => {
+
+    const [timer, setTimer] = useState(null)
+
+    const onClickHandler = (e) => {
+        e.stopPropagation()
+        console.log(e.target);
+        e.target.closest(".mapMarkerInner").classList.add("clicked")
+        setTimeout(() => {
+            e.target.closest(".mapMarkerInner").classList.remove("clicked")
+        }, 100)
+    }
+
+    const longTouch = (e) => {
+        e.target.closest(".mapMarkerInner").querySelector(".mapMarkerMore").classList.add("touch")
+        e.target.closest(".mapMarkerInner").querySelector(".close").classList.add("active")
+    }
+
+    const onTouchStartHandler = (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        setTimer(setTimeout(() => longTouch(e), 700))
+    }
+
+    const onTouchEndHandler = (e) => {
+        clearTimeout(timer)
+    }
+
+    const close = (e) => {
+        e.stopPropagation()
+        e.target.closest(".mapMarkerInner").querySelector(".mapMarkerMore").classList.remove("touch")
+        e.target.closest(".mapMarkerInner").querySelector(".close").classList.remove("active")
+    }
+
+    return (
+        <div className="mapMarkerWrapper">
+            <div onClick={onClickHandler} onTouchEnd={onTouchEndHandler} onTouchStart={onTouchStartHandler} className="mapMarkerInner">
+                <div className="mapMarkerInfo">
+                    <div className="mapMarkerContent">
+                        <div className="mapMarkerTitle">Название ивента</div>
+                        <div className="mapMarkerText">10.09.2022 10:00</div>
+                        <div className="mapMarkerMore">какая-то дополнительная информация об ивента</div>
+                    </div>
+                </div>
+                <div className="mapBottom">
+
+                </div>
+                <div className="close" onClick={close}>
+                    <Icon24Add width={15} height={15}/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/panels/map.js
+++ src/panels/map.js
@@ -1,23 +1,32 @@
-import { Group, PanelHeader } from "@vkontakte/vkui"
+import { Group, PanelHeader, Spinner } from "@vkontakte/vkui"
 import GoogleMapReact from 'google-map-react';
+import { useState } from "react";
+import { MapMarker } from "../components/mapMarker";
 
 export const MapPanel = () => {
+
+    const [events, setEvents] = useState([])
+
     return (
         <>
             <PanelHeader separator={false} className="shadowPanelHeader">
                 Карта событий
             </PanelHeader>
             <Group className="mapGroup">
-                <GoogleMapReact
+                {!events && <Spinner/>}
+                {events && <GoogleMapReact
                     bootstrapURLKeys={{ key: "AIzaSyBY3XkvjucuJx55bzFZMTJQnYH5JnWK8SQ" }}
                     defaultCenter={{
                         lat: 53.90478949220637,
                         lng: 27.54735640149058
                     }}
-                    defaultZoom={10}
+                    defaultZoom={17}
                 >
-
-                </GoogleMapReact>
+                    <MapMarker lat={53.90478949220637} lng={27.54735640149058} />
+                    <MapMarker lat={53.886469185056136} lng={27.53915628552071} />
+                    <MapMarker lat={53.887077641997124} lng={27.535944819314054} />
+                    <MapMarker lat={53.88911935332895} lng={27.536236386276723} />
+                </GoogleMapReact>}
             </Group>
         </>
     )
